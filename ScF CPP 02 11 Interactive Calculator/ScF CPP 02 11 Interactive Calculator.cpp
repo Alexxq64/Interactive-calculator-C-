3b83b4abb9c25c149415bb7b6d4d9e6c163cffa6
Alex Kuznetsov <alexxq@list.ru>
2023-01-12 00:44:20 +0300

    released for test

--- ScF CPP 02 11 Interactive Calculator/ScF CPP 02 11 Interactive Calculator.cpp
+++ ScF CPP 02 11 Interactive Calculator/ScF CPP 02 11 Interactive Calculator.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <bitset>
 #include <locale.h>
-#include <cstdlib>
 
 using namespace std;
 
@@ -12,47 +11,38 @@ char operation;
 long long operand1;
 int operand2;
 float result;
+string wrongOperation;
 
-int inputNumber(string);
-long long expo(int, int);
-long long factorial(long long);
-bool suchOperationExists(char);
+void showPrompt();
+char getOperation();
+int getNumber(string);
+__int64 expo(int, int);
+unsigned __int64 factorial(int);
+bool existsSuchOperation(char);
 float calculate(char, int, int);
-//void showBinary(char, int, int);
 bool isInteger(string);
 
-
-int main()
-{
+int main() {
     setlocale(LC_ALL, "Russian");
-    cout << "Калькулятор. Работает с целыми числами. Выполняет следующие операции:\n";
-    cout << "+ - * /     & | ^,\n";
-    cout << "а также факториал - f\n";
-    cout << "и возведение в степень - p.\n";
-    cout << "Выход - q.\n";
-	while (true) {
-        cout << endl << endl << "Выберите операцию: ";
-		do {
-			cin >> operation;
-            //cin.clear();
-		} while (!suchOperationExists(operation));
-		if (operation == 'q') {
-			cout << "До свидания";
-			return 0;
-		}
-		operand1 = inputNumber("Введите число : ");
-
-		if (operation == 'f')
-		{
-			cout << endl << operand1 << "! = ";
-			cout << factorial(operand1) << endl;
-		}
-		else
-		{
-            operand2 = inputNumber("Введите второе число: ");
+    showPrompt();
+    while (true) {
+        operation = getOperation();
+        if (operation == 'q') {
+            cout << "До свидания.";
+            return 0;
+        }
+        operand1 = getNumber("Введите число : ");
+        if (operation == 'f'){
+            if (operand1 < 0) cout << "Факториал отрицательных чисел запрещен." << endl;
+            else cout << endl << operand1 << "! = " << factorial(operand1) << endl;
+        }
+        else{
+            operand2 = getNumber("Введите второе число: ");
             cout << endl;
             result = calculate(operation, operand1, operand2);
-            cout << result << endl;
+            if (wrongOperation == "") 
+                cout << result << endl << endl;
+            else cout << wrongOperation;
             if (operation == '&' ||
                 operation == '|' ||
                 operation == '^') {
@@ -61,88 +51,114 @@ int main()
                 cout << "--------" << endl;
                 cout << bitset<8>(result) << endl;
             }
-		}
-	}
+        }
+    }
     return 0;
 }
 
-int inputNumber(string prompt) {
+void showPrompt(){
+    cout << "Калькулятор.\nРаботает с целыми числами.\nВыполняет следующие операции:\n";
+    cout << "+   -   *   /       &   |   ^ ,\n";
+    cout << "а также факториал - f\n";
+    cout << "и возведение в степень - p.\n";
+    cout << "Выход - q.\n";
+}
+
+char getOperation() {
+    string input;
+    cout << endl << "Выберите операцию: ";
+    while (true) {
+        getline(cin,input);
+        if ((input == "") ||
+            (input[1] != '\0') ||
+            !existsSuchOperation(input[0]))
+            cout << "Неправильный ввод. Попробуйте еще раз. \n";
+        else return input[0];
+    } 
+}
+
+int getNumber(string prompt){
     string input;
     cout << prompt;
     while (true) {
-        getline(cin,input);
-        if (input == "") cout << endl << "Test !!!!!" << endl;
-        if (isInteger(input)) return stoi(input);
-        else cout << "Неправильный ввод. Попробуйте еще раз. \n";
-    } 
-
+        getline(cin, input);
+        if ((input == "") ||
+            (!isInteger(input))) 
+            cout << "Неправильный ввод. Попробуйте еще раз. \n";
+        else {
+            return stoi(input);
+        }
+    }
 }
 
-bool suchOperationExists(char op) {
-    for (char o : operations)
-    {
+
+bool existsSuchOperation(char op) {
+    for (char o : operations){
         if (o == op) return true;
     }
-    cout << "Нет такой операции.\n";
     return false;
  }
 
 float calculate(char operation, int operand1, int operand2) {
+    wrongOperation = "";
     string explanation;
     explanation += operation;
     if (operation == 'p') explanation = "в степени";
     cout << operand1 << " " << explanation << " " << operand2 << " = ";
-    switch (operation)
-    {
+    switch (operation){
     case '+':
         return operand1 + operand2;
     case '-':
         return operand1 - operand2;
     case '*':
         return operand1 * operand2;
-    case '/':
-    {
-        if (operand2)
-        {
+    case '/':{
+        if (operand2){
             return operand1 / (float)operand2;
         }
-        else
-        {
-            cout << "Деление на ноль запрещено.\n";
+        else{
+            wrongOperation = "Деление на ноль запрещено.\n";
             return 0;
         }
     }
     case '&':
-        //showBinary(operation, operand1, operand2);
         return operand1 & operand2;
     case '|':
         return operand1 | operand2;
     case '^':
         return operand1 ^ operand2;
     case 'p':
-        return expo(operand1, operand2);
+        if (operand2 < 0) {
+            wrongOperation = "Калькулятор не умеет возводить в отрицательную степень.\n";
+            return 0;
+        }
+        else {
+            return expo(operand1, operand2);
+        }
     }
 
 }
 
-long long expo(int number, int power) {
+__int64 expo(int number, int power) {
     int  result = 1;
-    for (int i = 1; i <= power; i++)
-    {
+    for (int i = 1; i <= power; i++){
         result *= number;
     }
     return result;
 }
 
-long long factorial(long long n) {
-    return ((n == 1) || (n == 0)) ? 1 : n * factorial(n - 1);
+unsigned __int64 factorial(int n) {
+    float result = 1;
+    if (n == 0) return 1;
+    for (int i = 1; i <= n; i++)
+        result *= i;
+    return result;
 }
 
 bool isInteger(string input) {
     bool firstChar = true;
     if (input == "") return false;
-    for (char c : input)
-    {
+    for (char c : input){
         if (firstChar) {
             firstChar = false;
             if (!((c == '-') || (c >= '0' && c <= '9'))) return false;
@@ -153,8 +169,3 @@ bool isInteger(string input) {
     return true;
 }
 
-//void showBinary(char operation, int operand1, int operand2) {
-//    cout << bitset<8>(operand1) << " " << operation << endl;
-//    cout << bitset<8>(operand2) << endl;
-//
-//}
